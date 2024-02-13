db492a7dad314c15cbeeb3aca25483741043249d
GauravGothi <gauravgothi.patidar@gmail.com>
2024-02-13 15:10:21 +0530

    initial commit

---
+++ src/main/java/in/co/mpwin/vigilencebilling/VigilenceBillingApplication.java
@@ -0,0 +1,13 @@
+package in.co.mpwin.vigilencebilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigilenceBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigilenceBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/co/mpwin/vigilencebilling/VigilenceBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.mpwin.vigilencebilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigilenceBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
